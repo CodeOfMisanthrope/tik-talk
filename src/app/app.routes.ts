561205396d46d3ce027f9c27f83923361e0a407f
Ilya <iosdsbb@gmail.com>
2025-11-19 13:08:53 +0300

    feat(pages): added login-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {SearchPageComponent} from './pages/search-page/search-page.component';
 import {LayoutComponent} from './common-ui/layout/layout.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
   {
@@ -9,4 +10,5 @@ export const routes: Routes = [
     ],
     // canActivate: [canActivateAuth]
   },
+  {path: 'login', component: LoginPageComponent},
 ];
