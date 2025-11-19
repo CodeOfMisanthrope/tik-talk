f18fd5401bb03571098b0e3c89ef63f91da92100
Ilya <iosdsbb@gmail.com>
2025-11-19 12:07:29 +0300

    feat: added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {SearchPageComponent} from './pages/search-page/search-page.component';
+import {LayoutComponent} from './common-ui/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      {path: '', component: SearchPageComponent},
+    ],
+    // canActivate: [canActivateAuth]
+  },
+];
