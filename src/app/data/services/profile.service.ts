2e377be4aa5c44d09428fb75dd3f496ef8f50a67
Ilya <iosdsbb@gmail.com>
2025-11-19 12:07:17 +0300

    feat(services): added profile.service

---
+++ src/app/data/services/profile.service.ts
@@ -0,0 +1,20 @@
+import {inject, Injectable} from '@angular/core';
+import {Profile} from '../interfaces/profile.interface';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  http = inject(HttpClient);
+
+  baseApiUrl = 'https://icherniakov.ru/yt-course/';
+
+  getTestAccounts() {
+    return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
+  }
+
+  getMe() {
+    return this.http.get<Profile>(`${this.baseApiUrl}account/me`);
+  }
+}
