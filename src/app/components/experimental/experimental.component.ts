812ca2d9e73b71e9b66604fff545aef52f13cbc7
Ilya <iosdsbb@gmail.com>
2025-11-20 16:55:21 +0300

    feat(components): updated experimental

--- src/app/components/experimental/experimental.component.ts
+++ src/app/components/experimental/experimental.component.ts
@@ -9,24 +9,24 @@ import {combineLatest, fromEvent, interval, map, of, zip} from 'rxjs';
 })
 export class ExperimentalComponent {
   constructor() {
-    const observable$ = zip([
-      interval(3000).pipe(map(i => "1_" + i)),
-      interval(200).pipe(map(i => "2_" + i)),
-      fromEvent(document.body, 'click')
-    ]);
-
-    observable$.subscribe({
-      next: val => {
-        console.log("next: ", val);
-      },
-
-      error: err => {
-        console.log("error: ", err);
-      },
-
-      complete: () => {
-        console.log("complete");
-      }
-    })
+    // const observable$ = zip([
+    //   interval(3000).pipe(map(i => "1_" + i)),
+    //   interval(200).pipe(map(i => "2_" + i)),
+    //   fromEvent(document.body, 'click')
+    // ]);
+    //
+    // observable$.subscribe({
+    //   next: val => {
+    //     console.log("next: ", val);
+    //   },
+    //
+    //   error: err => {
+    //     console.log("error: ", err);
+    //   },
+    //
+    //   complete: () => {
+    //     console.log("complete");
+    //   }
+    // })
   }
 }
