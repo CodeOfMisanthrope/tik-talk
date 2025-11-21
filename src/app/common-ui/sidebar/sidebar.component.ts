f01b6ce074838eec096418fb709fcb041702da51
Ilya <iosdsbb@gmail.com>
2025-11-21 20:47:25 +0300

    feat(common-ui): show subscribers on sidebar

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {NgForOf} from '@angular/common';
+import {AsyncPipe, JsonPipe, NgForOf} from '@angular/common';
 import {SvgIconComponent} from '../svg-icon/svg-icon.component';
+import {ProfileService} from '../../data/services/profile.service';
+import {SubscriberCardComponent} from './subsriber-card/subscriber-card.component';
 
 @Component({
   selector: 'app-sidebar',
   imports: [
     NgForOf,
     RouterLink,
-    SvgIconComponent
+    SvgIconComponent,
+    SubscriberCardComponent,
+    AsyncPipe,
+    JsonPipe
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  profileService = inject(ProfileService);
+
+  subscribers$ = this.profileService.getSubscribersShortList()
+
+  // me = this.profileService.getMe();
+
   menuItems = [
     {
       label: 'Моя страница',
