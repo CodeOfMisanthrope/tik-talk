1c58527b65503492d36d65b81be23995732ddbfd
Ilya <iosdsbb@gmail.com>
2025-11-22 10:25:34 +0300

    feat(common-ui): added profile-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
-import {SearchPageComponent} from './pages/search-page/search-page.component';
 import {LayoutComponent} from './common-ui/layout/layout.component';
 import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {ProfilePageComponent} from './pages/profile-page/profile-page.component';
+import {SearchPageComponent} from './pages/search-page/search-page.component';
 import {canActivateAuth} from './auth/access.guard';
 
 export const routes: Routes = [
   {
     path: '', component: LayoutComponent, children: [
-      {path: '', component: SearchPageComponent},
+      {path: 'search', component: SearchPageComponent},
+      {path: 'profile/:id', component: ProfilePageComponent},
     ],
     canActivate: [canActivateAuth]
   },
