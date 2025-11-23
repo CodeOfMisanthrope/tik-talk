88682012c0c264afd2c70e58c282408d5fb077d6
Ilya <iosdsbb@gmail.com>
2025-11-23 15:47:36 +0300

    feat: added route settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {LayoutComponent} from './common-ui/layout/layout.component';
 import {LoginPageComponent} from './pages/login-page/login-page.component';
 import {ProfilePageComponent} from './pages/profile-page/profile-page.component';
 import {SearchPageComponent} from './pages/search-page/search-page.component';
+import {SettingsPageComponent} from './pages/settings-page/settings-page.component';
 import {canActivateAuth} from './auth/access.guard';
 
 export const routes: Routes = [
@@ -10,6 +11,7 @@ export const routes: Routes = [
     path: '', component: LayoutComponent, children: [
       {path: 'search', component: SearchPageComponent},
       {path: 'profile/:id', component: ProfilePageComponent},
+      {path: 'settings', component: SettingsPageComponent},
     ],
     canActivate: [canActivateAuth]
   },
