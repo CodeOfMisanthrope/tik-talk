5780875d6149bd91239fb78623d79e71ed14780d
Ilya <iosdsbb@gmail.com>
2025-11-25 10:10:28 +0300

    feat: updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,9 +9,10 @@ import {canActivateAuth} from './auth/access.guard';
 export const routes: Routes = [
   {
     path: '', component: LayoutComponent, children: [
-      {path: 'search', component: SearchPageComponent},
+      {path: '', redirectTo: 'profile/me', pathMatch: 'full'},
       {path: 'profile/:id', component: ProfilePageComponent},
       {path: 'settings', component: SettingsPageComponent},
+      {path: 'search', component: SearchPageComponent},
     ],
     canActivate: [canActivateAuth]
   },
