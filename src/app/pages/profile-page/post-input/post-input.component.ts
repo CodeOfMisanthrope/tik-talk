970500bba7639411096270c365cd2ba73b739a21
Ilya <iosdsbb@gmail.com>
2025-11-25 20:08:54 +0300

    feat(post-input): added onCreatePost

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,26 +1,44 @@
 import {Component, inject, Renderer2} from '@angular/core';
-import {AvatarCircleComponent} from '../../../common-ui/avatar-circle/avatar-circle.component';
-import {ProfileService} from '../../../data/services/profile.service';
 import {NgIf} from '@angular/common';
+import {AvatarCircleComponent} from '../../../common-ui/avatar-circle/avatar-circle.component';
 import {SvgIconComponent} from '../../../common-ui/svg-icon/svg-icon.component';
+import {ProfileService} from '../../../data/services/profile.service';
+import {PostService} from '../../../data/services/post.service';
+import {FormsModule} from '@angular/forms';
+import {firstValueFrom} from 'rxjs';
 
 @Component({
   selector: 'app-post-input',
   imports: [
     AvatarCircleComponent,
     NgIf,
-    SvgIconComponent
+    SvgIconComponent,
+    FormsModule
   ],
   templateUrl: './post-input.component.html',
   styleUrl: './post-input.component.scss',
 })
 export class PostInputComponent {
- r2 = inject(Renderer2);
- profile = inject(ProfileService).me;
+  r2 = inject(Renderer2);
+  postService = inject(PostService);
+  profile = inject(ProfileService).me;
+
+  postText = "";
 
   onTextareaInput(event: Event) {
     const textarea = event.target as HTMLTextAreaElement;
     this.r2.setStyle(textarea, 'height', 'auto');
     this.r2.setStyle(textarea, 'height', textarea.scrollHeight + 'px');
+  }
+
+  onCreatePost() {
+    if (!this.postText) return;
+
+    firstValueFrom(this.postService.createPost({
+      title: "",
+      content: this.postText,
+      authorId: this.profile()!.id,
+      communityId: 0,
+    }))
   }
 }
