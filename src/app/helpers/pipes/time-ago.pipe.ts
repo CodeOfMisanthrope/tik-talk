560421ef4e6efaf440055f6b1cf5e3516c699058
Ilya <iosdsbb@gmail.com>
2025-11-26 17:47:10 +0300

    feat(time-ago.pipe): updated

--- src/app/helpers/pipes/time-ago.pipe.ts
+++ src/app/helpers/pipes/time-ago.pipe.ts
@@ -1,18 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {calcDiffTime} from '../../utils/date';
-// import {convertToValidDateStr} from '../../utils/date';
+import {timeAgo} from '../../utils/date';
 
 @Pipe({
   name: 'timeAgo'
 })
 export class TimeAgoPipe implements PipeTransform {
-  transform(value: string | null, ...args: unknown[]): unknown {
+  transform(value: string | null, ...args: unknown[]): string | null {
     if (!value) return null;
-    // console.log(value);
-    // convertToValidDateStr(value);
+
     const date = new Date(value);
-    // console.log(date);
-    calcDiffTime(date);
-    return value;
+    return timeAgo(date);
   }
 }
