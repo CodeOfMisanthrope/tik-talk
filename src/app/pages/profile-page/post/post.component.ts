ea96f127194a2b22536d0316d918ee932c7868a8
Ilya <iosdsbb@gmail.com>
2025-11-26 11:01:34 +0300

    feat(post): added show avatar, content, comment counter

--- src/app/pages/profile-page/post/post.component.ts
+++ src/app/pages/profile-page/post/post.component.ts
@@ -1,9 +1,16 @@
 import {Component, input} from '@angular/core';
 import {Post} from '../../../data/interfaces/post.interface';
+import {AvatarCircleComponent} from '../../../common-ui/avatar-circle/avatar-circle.component';
+import {DatePipe} from '@angular/common';
+import {SvgIconComponent} from '../../../common-ui/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [
+    DatePipe,
+    AvatarCircleComponent,
+    SvgIconComponent,
+  ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
