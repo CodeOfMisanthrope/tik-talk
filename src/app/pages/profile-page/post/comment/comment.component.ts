9e6389c0ca9a67414de699fbd265c78612d840bd
Ilya <iosdsbb@gmail.com>
2025-11-26 12:55:50 +0300

    feat(comment): added comment data show

--- src/app/pages/profile-page/post/comment/comment.component.ts
+++ src/app/pages/profile-page/post/comment/comment.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {Comment} from '../../../../data/interfaces/post.interface';
+import {AvatarCircleComponent} from '../../../../common-ui/avatar-circle/avatar-circle.component';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-comment',
-  imports: [],
+  imports: [
+    AvatarCircleComponent,
+    DatePipe
+  ],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
 })
 export class CommentComponent {
-
+  comment = input<Comment>();
 }
