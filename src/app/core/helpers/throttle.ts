e9e0e1037233b23449ecab2c9722f174ad679cd9
Ilya <iosdsbb@gmail.com>
2025-11-27 23:01:04 +0300

    feat(core/helpers): added throttle

---
+++ src/app/core/helpers/throttle.ts
@@ -0,0 +1,24 @@
+export function throttle<T extends Function>(func: T, wait: number) {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+  let lastArgs: unknown[] | null = null;
+
+  return function <This>(this: This, ...args: unknown[]) {
+    if (!timeout) {
+      func.call(this, ...args);
+    } else {
+      lastArgs = [...args];
+    }
+
+    timeout = setTimeout(() => {
+      if (lastArgs) {
+        func.call(this, ...lastArgs);
+        lastArgs = null;
+      }
+
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+    }, wait);
+  };
+}
