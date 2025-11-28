f6e0b7f4178ab21cce2cfb8435fb68755d9a5612
Ilya <iosdsbb@gmail.com>
2025-11-28 12:24:09 +0300

    feat(core/decorators): fix throttle

--- src/app/core/decorators/throttle.ts
+++ src/app/core/decorators/throttle.ts
@@ -1,31 +1,10 @@
+import {throttle} from '../helpers/throttle';
+
 export function Throttle(wait: number) {
   return function (target: any, key: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
 
-    let timeout: ReturnType<typeof setTimeout> | null = null;
-    let lastArgs: unknown[] | null = null;
-
-    descriptor.value = function (...args: any[]) {
-      if (!timeout) {
-        originalMethod.call(this, ...args);
-
-      } else {
-        lastArgs = [...args];
-        return;
-      }
-
-      timeout = setTimeout(() => {
-        if (lastArgs) {
-          originalMethod.call(this, ...lastArgs);
-          lastArgs = null;
-        }
-
-        if (timeout) {
-          clearTimeout(timeout);
-          timeout = null;
-        }
-      }, wait);
-    };
+    descriptor.value = throttle(originalMethod, wait);
 
     return descriptor;
   };
