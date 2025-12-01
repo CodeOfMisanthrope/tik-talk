b56d4b398caa7a7cd281b8ba0a222f81c4c55196
Ilya <iosdsbb@gmail.com>
2025-12-01 07:53:54 +0300

    feat(chats): added ChatService

---
+++ src/app/data/services/chats.service.ts
@@ -0,0 +1,34 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Chat, Message} from '../interfaces/chats.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatsService {
+  http = inject(HttpClient);
+
+  baseApiUrl = 'https://icherniakov.ru/yt-course/';
+  chatsUrl = `${this.baseApiUrl}chat/`;
+  messageUrl = `${this.baseApiUrl}message/`;
+
+  createChat(userId: number) {
+    return this.http.post<Chat>(`${this.chatsUrl}${userId}`, {});
+  }
+
+  getMyChats(userId: number) {
+    return this.http.get<Chat[]>(`${this.chatsUrl}get_my_chats/`);
+  }
+
+  getChatById(chatId: number) {
+    return this.http.get<Chat>(`${this.chatsUrl}${chatId}`);
+  }
+
+  sendMessage(chatId: number, message: string) {
+    return this.http.post<Message>(`${this.messageUrl}${chatId}`, {}, {
+      params: {
+        message
+      }
+    });
+  }
+}
