ba607e803b897a6a8075493a42d80ce308745de5
Ilya <iosdsbb@gmail.com>
2025-12-01 09:20:49 +0300

    types(chats-list): added btn chats list

--- src/app/pages/chats-page/chats-list/chats-list.component.ts
+++ src/app/pages/chats-page/chats-list/chats-list.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {ChatsBtnComponent} from '../chats-btn/chats-btn.component';
+import {ChatsService} from '../../../data/services/chats.service';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-chats-list',
   imports: [
-    ChatsBtnComponent
+    ChatsBtnComponent,
+    AsyncPipe
   ],
   templateUrl: './chats-list.component.html',
   styleUrl: './chats-list.component.scss',
 })
 export class ChatsListComponent {
+  chatsService = inject(ChatsService);
 
+  chats$ = this.chatsService.getMyChats();
 }
