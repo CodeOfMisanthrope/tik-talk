f63d277c69626b1f97e4d5fd29aad51cf451c268
Ilya <iosdsbb@gmail.com>
2025-12-01 05:41:55 +0300

    feat(chat-list): init

---
+++ src/app/pages/chats-page/chats-list/chats-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {ChatsBtnComponent} from '../chats-btn/chats-btn.component';
+
+@Component({
+  selector: 'app-chats-list',
+  imports: [
+    ChatsBtnComponent
+  ],
+  templateUrl: './chats-list.component.html',
+  styleUrl: './chats-list.component.scss',
+})
+export class ChatsListComponent {
+
+}
