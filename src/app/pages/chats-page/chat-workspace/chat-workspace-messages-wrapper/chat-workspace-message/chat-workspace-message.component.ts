8e07b6734b70abde8fe4d0b35e6514f1bf06697b
Ilya <iosdsbb@gmail.com>
2025-12-03 13:35:26 +0300

    feat(chat-workspace-message): show message info

--- src/app/pages/chats-page/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-message/chat-workspace-message.component.ts
+++ src/app/pages/chats-page/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-message/chat-workspace-message.component.ts
@@ -1,9 +1,14 @@
 import {Component, input} from '@angular/core';
 import {Message} from '../../../../../data/interfaces/chats.interface';
+import {AvatarCircleComponent} from '../../../../../common-ui/avatar-circle/avatar-circle.component';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-chat-workspace-message',
-  imports: [],
+  imports: [
+    AvatarCircleComponent,
+    DatePipe
+  ],
   templateUrl: './chat-workspace-message.component.html',
   styleUrl: './chat-workspace-message.component.scss',
 })
