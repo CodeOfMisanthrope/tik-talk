5ce3d8d0fca959759270dc5d6e2f4db4e62345de
Ilya <iosdsbb@gmail.com>
2025-12-03 10:47:19 +0300

    feat(message-input): added event

--- src/app/common-ui/message-input/message-input.component.ts
+++ src/app/common-ui/message-input/message-input.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, HostBinding, inject, input, Output, Renderer2} from '@angular/core';
+import {AvatarCircleComponent} from '../avatar-circle/avatar-circle.component';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {SvgIconComponent} from '../svg-icon/svg-icon.component';
+import {PostService} from '../../data/services/post.service';
+import {ProfileService} from '../../data/services/profile.service';
+import {Deprecated} from '../../core/decorators/deprecated';
+import {firstValueFrom} from 'rxjs';
 
 @Component({
   selector: 'app-message-input',
-  imports: [],
+  imports: [
+    AvatarCircleComponent,
+    FormsModule,
+    NgIf,
+    SvgIconComponent
+  ],
   templateUrl: './message-input.component.html',
   styleUrl: './message-input.component.scss',
 })
 export class MessageInputComponent {
+  r2 = inject(Renderer2);
+  me = inject(ProfileService).me;
 
+  @Output() created = new EventEmitter<string>();
+
+  postText = "";
+
+  onTextareaInput(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+    this.r2.setStyle(textarea, 'height', 'auto');
+    this.r2.setStyle(textarea, 'height', textarea.scrollHeight + 'px');
+  }
+
+  onCreatePost() {
+    if (!this.postText) return;
+
+    this.created.emit(this.postText);
+    this.postText = "";
+  }
 }
