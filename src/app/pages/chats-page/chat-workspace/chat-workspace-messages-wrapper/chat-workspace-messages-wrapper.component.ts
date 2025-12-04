4c40a480011d6a4b1a099229e40c84f1acb9cf1c
Ilya <iosdsbb@gmail.com>
2025-12-04 06:38:36 +0300

    feat(chat-workspace-messages-wrapper): added logic with update messages

--- src/app/pages/chats-page/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
+++ src/app/pages/chats-page/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
@@ -1,9 +1,9 @@
-import {Component, inject, input, signal} from '@angular/core';
+import {Component, inject, input, OnInit, signal} from '@angular/core';
 import {ChatWorkspaceMessageComponent} from './chat-workspace-message/chat-workspace-message.component';
 import {MessageInputComponent} from '../../../../common-ui/message-input/message-input.component';
 import {ChatsService} from '../../../../data/services/chats.service';
 import {Chat, Message} from '../../../../data/interfaces/chats.interface';
-import {firstValueFrom} from 'rxjs';
+import {firstValueFrom, interval, timer} from 'rxjs';
 
 @Component({
   selector: 'app-chat-workspace-messages-wrapper',
@@ -14,12 +14,20 @@ import {firstValueFrom} from 'rxjs';
   templateUrl: './chat-workspace-messages-wrapper.component.html',
   styleUrl: './chat-workspace-messages-wrapper.component.scss',
 })
-export class ChatWorkspaceMessagesWrapperComponent {
+export class ChatWorkspaceMessagesWrapperComponent implements OnInit {
   chatsService = inject(ChatsService);
 
   chat = input.required<Chat>();
 
   messages = this.chatsService.activeChatMessages;
+
+  ngOnInit() {
+    interval(10000).subscribe(() => {
+      console.log('updated messages')
+      this.chatsService.getChatById(this.chat().id);
+      console.log(this.messages());
+    });
+  }
 
   async onSendMessage(messageText: string) {
     await firstValueFrom(this.chatsService.sendMessage(this.chat().id, messageText));
