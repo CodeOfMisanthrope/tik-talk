de456a5ae094630946ba5c65e8495f1a40c7dc43
Ilya <iosdsbb@gmail.com>
2025-12-04 13:19:32 +0300

    feat(chat-workspace-messages-wrapper): added logic with show time zone messages

--- src/app/pages/chats-page/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
+++ src/app/pages/chats-page/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
@@ -1,9 +1,10 @@
-import {Component, inject, input, OnInit, signal} from '@angular/core';
+import {Component, computed, inject, input, OnInit, signal} from '@angular/core';
 import {ChatWorkspaceMessageComponent} from './chat-workspace-message/chat-workspace-message.component';
 import {MessageInputComponent} from '../../../../common-ui/message-input/message-input.component';
 import {ChatsService} from '../../../../data/services/chats.service';
 import {Chat, Message} from '../../../../data/interfaces/chats.interface';
 import {firstValueFrom, interval, timer} from 'rxjs';
+import {groupMessagesByTimeZone} from '../../../../utils/date';
 
 @Component({
   selector: 'app-chat-workspace-messages-wrapper',
@@ -21,17 +22,28 @@ export class ChatWorkspaceMessagesWrapperComponent implements OnInit {
 
   messages = this.chatsService.activeChatMessages;
 
+  groupsMessages = computed(() => groupMessagesByTimeZone(this.messages()));
+
   ngOnInit() {
+    // console.log(this.messages());
     interval(10000).subscribe(() => {
-      console.log('updated messages')
+      // console.log('updated messages')
       this.chatsService.getChatById(this.chat().id);
-      console.log(this.messages());
+      // console.log(this.messages());
     });
+
+    console.log(this.groupsMessages);
+    // this.groupMessagesByTimeZone();
   }
 
   async onSendMessage(messageText: string) {
     await firstValueFrom(this.chatsService.sendMessage(this.chat().id, messageText));
 
     await firstValueFrom(this.chatsService.getChatById(this.chat().id));
+  }
+
+  groupMessagesByTimeZone() {
+    const groups = groupMessagesByTimeZone(this.messages());
+    console.log(groups);
   }
 }
