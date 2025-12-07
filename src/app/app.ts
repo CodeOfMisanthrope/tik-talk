8f1053ff1fc179306deb45ad88ddf008bc07c2db
Ilya <iosdsbb@gmail.com>
2025-12-07 19:46:52 +0300

    feat(app): added experimental-form-my

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ExperimentalComponent} from './components/experimental/experimental.component';
+import {ExperimentalFormMyComponent} from './components/experimental-form-my/experimental-form-my.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    // ExperimentalComponent
+    ExperimentalComponent,
+    ExperimentalFormMyComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
