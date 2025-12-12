ceb0f6b6e16e27cbbb0db94555f5f82ef5d020f6
Ilya <iosdsbb@gmail.com>
2025-12-12 12:56:30 +0300

    feat(libs/profile): added feature-profile-list

--- libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
+++ libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
@@ -1,9 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { ProfileCard } from '../../common-ui/profile-card/profile-card.component';
+import { ProfileCard } from '../../../../../../libs/profile/src/lib/ui/profile-card/profile-card.component';
 // import { Profile } from '../../data/interfaces/profile.interface';
 // import { ProfileService } from '../../data/services/profile.service';
 import {ProfileService} from '@tt/profile';
-import { ProfileFiltersComponent } from './profile-filters/profile-filters.component';
+import { ProfileFiltersComponent } from '../profile-filters/profile-filters.component';
 
 @Component({
   selector: 'app-search-page',
