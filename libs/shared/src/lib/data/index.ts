d9ff7c78d2a6e17ca1df806a1b041761ea34aaf5
Ilya <iosdsbb@gmail.com>
2025-12-12 17:42:04 +0300

    feat(libs/shared): added GlobalStoreService

--- libs/shared/src/lib/data/index.ts
+++ libs/shared/src/lib/data/index.ts
@@ -1,5 +1,8 @@
 import { Pageable } from './interfaces/pageable.interface';
+import { GlobalStoreService } from './services/global-store.service';
 
 export {
-  type Pageable
+  type Pageable,
+
+  GlobalStoreService,
 };
