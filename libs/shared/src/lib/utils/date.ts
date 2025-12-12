fcdd4afa511808aca99de21dbe8f2a3c5912c10f
Ilya <iosdsbb@gmail.com>
2025-12-12 13:51:54 +0300

    refactor(libs/shared): moved date

--- libs/shared/src/lib/utils/date.ts
+++ libs/shared/src/lib/utils/date.ts
@@ -2,7 +2,8 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ru';
 // import { Message } from '../data/interfaces/chats.interface';
-// import { DateTime } from 'luxon';
+import {Message} from '@tt/chats';
+import { DateTime } from 'luxon';
 
 export function timeAgo(date: Date) {
   return getDiffTime(date);
@@ -14,33 +15,33 @@ export function getDiffTime(date: Date) {
   return dayjs().to(date);
 }
 
-// export function groupMessagesByTimeZone(messages: Message[]) {
-//   const groups = new Map<string, Message[]>();
-//
-//   const today = DateTime.now().startOf('day');
-//   const yesterday = today.minus({ days: 1 });
-//
-//   messages.forEach((message) => {
-//     const messageDate = DateTime.fromISO(message.createdAt, { zone: 'UTC' })
-//       .setZone(DateTime.local().zone)
-//       .startOf('day');
-//
-//     let dateLabel: string;
-//
-//     if (messageDate.equals(today)) {
-//       dateLabel = 'Сегодня';
-//     } else if (messageDate.equals(yesterday)) {
-//       dateLabel = 'Вчера';
-//     } else {
-//       dateLabel = messageDate.toFormat('MM.dd.yyyy');
-//     }
-//
-//     if (!groups.has(dateLabel)) {
-//       groups.set(dateLabel, []);
-//     }
-//
-//     groups.get(dateLabel)?.push(message);
-//   });
-//
-//   return Array.from(groups.entries());
-// }
+export function groupMessagesByTimeZone(messages: Message[]) {
+  const groups = new Map<string, Message[]>();
+
+  const today = DateTime.now().startOf('day');
+  const yesterday = today.minus({ days: 1 });
+
+  messages.forEach((message) => {
+    const messageDate = DateTime.fromISO(message.createdAt, { zone: 'UTC' })
+      .setZone(DateTime.local().zone)
+      .startOf('day');
+
+    let dateLabel: string;
+
+    if (messageDate.equals(today)) {
+      dateLabel = 'Сегодня';
+    } else if (messageDate.equals(yesterday)) {
+      dateLabel = 'Вчера';
+    } else {
+      dateLabel = messageDate.toFormat('MM.dd.yyyy');
+    }
+
+    if (!groups.has(dateLabel)) {
+      groups.set(dateLabel, []);
+    }
+
+    groups.get(dateLabel)?.push(message);
+  });
+
+  return Array.from(groups.entries());
+}
