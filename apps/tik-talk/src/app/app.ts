a4104fdc47edcc305e33b718a503bf39fac76d68
Ilya <iosdsbb@gmail.com>
2025-12-13 12:07:17 +0300

    refactor(app): removed unused code

--- apps/tik-talk/src/app/app.ts
+++ apps/tik-talk/src/app/app.ts
@@ -1,18 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ExperimentalFromTvComponent } from '../../../../libs/experimental/src/lib/feature-form-tv/experimental-form-tv/experimental-from-tv.component';
-import { ExperimentalFormMyComponent } from './components/experimental-form-my/experimental-form-my.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    // ExperimentalComponent,
-    ExperimentalFormMyComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('tik-talk');
+
 }
