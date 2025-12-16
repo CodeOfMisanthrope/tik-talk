8acce297d87ef6b42a60e24c3ddfa5295a1c30b1
Ilya <iosdsbb@gmail.com>
2025-12-16 12:59:28 +0300

    feat(posts): added store for post create

--- libs/posts/src/lib/data/store/actions.ts
+++ libs/posts/src/lib/data/store/actions.ts
@@ -1,10 +1,11 @@
 import {createActionGroup, emptyProps, props} from '@ngrx/store';
-import {Post} from '../interfaces/post.interface';
+import {Post, PostCreateDto} from '../interfaces/post.interface';
 
 export const postsActions = createActionGroup({
   source: 'posts',
   events: {
     'posts fetch': emptyProps(),
     'posts loaded': props<{posts: Post[]}>(),
+    'post create': props<{post: PostCreateDto}>(),
   }
 });
--- libs/posts/src/lib/data/store/effects.ts
+++ libs/posts/src/lib/data/store/effects.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
-import {firstValueFrom, map, switchMap} from 'rxjs';
+import {map, switchMap} from 'rxjs';
 import {PostService} from '../services/post.service';
 import {postsActions} from './actions';
 
@@ -12,13 +12,17 @@ export class PostsEffects {
   actions$ = inject(Actions);
 
   loadPosts = createEffect(() => {
-    console.log('called'); // выведется 1 раз
     return this.actions$.pipe(
       ofType(postsActions.postsFetch),
-      switchMap(() => {
-        console.log('fetchPosts'); // выведется 21 раз
-        return this.postsService.fetchPosts()
-      }),
+      switchMap(() => this.postsService.fetchPosts()),
+      map((res) => postsActions.postsLoaded({posts: res}))
+    );
+  });
+
+  postCreate = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(postsActions.postCreate),
+      switchMap(({post}) => this.postsService.createPost(post)),
       map((res) => postsActions.postsLoaded({posts: res}))
     );
   });
