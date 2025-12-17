02fde8ff74b02bf7c668cf120498b351ef1d7c1f
Ilya <iosdsbb@gmail.com>
2025-12-17 13:40:14 +0300

    types(chats): added ChatWsNativeService

---
+++ libs/chats/src/lib/data/services/chat-ws-native.service.ts
@@ -0,0 +1,33 @@
+import {ChatConnectionWSParams, ChatWsService} from '../interfaces/chat-ws-service.interface';
+
+export class ChatWsNativeService implements ChatWsService {
+
+  #socket: WebSocket | null = null;
+
+  connect(params: ChatConnectionWSParams) {
+    if (this.#socket) return;
+
+    this.#socket = new WebSocket(params.url, [params.token]);
+
+    this.#socket.onmessage = (event: MessageEvent) => {
+      params.handleMessage(JSON.parse(event.data));
+    };
+
+    this.#socket.onclose = () => {
+      console.log('А чо это вы тут делаете? Кино - то давно уже кончилось!');
+    };
+  }
+
+  sendMessage(text: string, chatId: number): void {
+    this.#socket?.send(
+      JSON.stringify({
+        text: text,
+        chat_id: chatId
+      })
+    );
+  }
+
+  disconnect() {
+    this.#socket?.close();
+  }
+}
