9afd334ceaf2d8ecfcde5cdeb98fa08c36310f0e
Ilya <iosdsbb@gmail.com>
2025-12-17 13:40:54 +0300

    feat(chats): usage ChatWsNativeService

--- libs/chats/src/lib/feature-chats-workspace/chats-page/chats-page.component.ts
+++ libs/chats/src/lib/feature-chats-workspace/chats-page/chats-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatsListComponent } from '../chats-list/chats-list.component';
+import {ChatsService} from '../../data';
 
 @Component({
   selector: 'app-chats-page',
@@ -8,4 +9,10 @@ import { ChatsListComponent } from '../chats-list/chats-list.component';
   templateUrl: './chats-page.component.html',
   styleUrl: './chats-page.component.scss',
 })
-export class ChatsPageComponent {}
+export class ChatsPageComponent implements OnInit {
+  #chatService = inject(ChatsService);
+
+  ngOnInit() {
+    this.#chatService.connectWs();
+  }
+}
