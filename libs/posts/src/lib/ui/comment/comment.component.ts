4d1fa6f0d82d60d4271a4462d121712a3474edbd
Ilya <iosdsbb@gmail.com>
2025-12-22 01:11:40 +0300

    style(posts): fmt comment

--- libs/posts/src/lib/ui/comment/comment.component.ts
+++ libs/posts/src/lib/ui/comment/comment.component.ts
@@ -1,14 +1,14 @@
-import {ChangeDetectionStrategy, Component, input} from '@angular/core';
-import {PostComment} from '../../data';
-import {AvatarCircleComponent} from '@tt/common-ui';
-import {TimeAgoPipe} from '../../pipes/time-ago.pipe';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { PostComment } from '../../data';
+import { AvatarCircleComponent } from '@tt/common-ui';
+import { TimeAgoPipe } from '../../pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-comment',
   imports: [AvatarCircleComponent, TimeAgoPipe],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommentComponent {
   comment = input<PostComment>();
