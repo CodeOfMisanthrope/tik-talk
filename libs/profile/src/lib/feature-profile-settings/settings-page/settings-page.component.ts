916724ebbdf20bc32d2ed4c0add570651dd67a2e
Ilya <iosdsbb@gmail.com>
2025-12-22 00:06:30 +0300

    perf(profile): settings-page switched to OnPush

--- libs/profile/src/lib/feature-profile-settings/settings-page/settings-page.component.ts
+++ libs/profile/src/lib/feature-profile-settings/settings-page/settings-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, ViewChild } from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, inject, ViewChild} from '@angular/core';
 import {
   FormBuilder,
   ReactiveFormsModule,
@@ -13,6 +13,7 @@ import { AvatarUploadComponent } from '../../ui/avatar-upload/avatar-upload.comp
   imports: [ReactiveFormsModule, AvatarUploadComponent],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SettingsPageComponent {
   fb = inject(FormBuilder);
