fdc64afb38cdc94c6438f9435301394792db7cd5
Ilya <iosdsbb@gmail.com>
2025-12-23 15:46:26 +0300

    feat(common-ui): added component infinite-scroll-trigger

--- libs/common-ui/src/lib/components/index.ts
+++ libs/common-ui/src/lib/components/index.ts
@@ -1,9 +1,11 @@
 import { AvatarCircleComponent } from './avatar-circle/avatar-circle.component';
+import { InfiniteScrollTriggerComponent } from './infinite-scroll-trigger/infinite-scroll-trigger.component';
 import { MessageCountComponent } from './message-count/message-count.component';
 import { SvgIconComponent } from './svg-icon/svg-icon.component';
 
 export {
   AvatarCircleComponent,
+  InfiniteScrollTriggerComponent,
   MessageCountComponent,
   SvgIconComponent
 };
