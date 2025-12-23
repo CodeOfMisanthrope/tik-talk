f5f3f7b5311485d086d40c1e70f89b579c3573f5
Ilya <iosdsbb@gmail.com>
2025-12-23 15:05:17 +0300

    feat(profile): updated store for pageable

--- libs/profile/src/lib/data/store/effects.ts
+++ libs/profile/src/lib/data/store/effects.ts
@@ -1,8 +1,10 @@
 import {inject, Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
-import {map, switchMap} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {map, switchMap, withLatestFrom} from 'rxjs';
 import {ProfileService} from '../services/profile.service';
 import {profileActions} from './actions';
+import {selectedProfileFilterParams, selectProfilePageable} from './selectors';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +12,23 @@ import {profileActions} from './actions';
 export class ProfileEffects {
   profileService = inject(ProfileService);
   actions$ = inject(Actions);
+  store = inject(Store);
 
   filterProfiles = createEffect(() => {
     return this.actions$.pipe(
-      ofType(profileActions.filterEvents),
-      switchMap(({ filters }) => {
-        return this.profileService.filterProfiles(filters);
+      ofType(
+        profileActions.filterEvents,
+        profileActions.setPage
+      ),
+      withLatestFrom(
+        this.store.select(selectedProfileFilterParams),
+        this.store.select(selectProfilePageable),
+      ),
+      switchMap(([_, filters, pageable]) => {
+        return this.profileService.filterProfiles({
+          ...pageable,
+          ...filters
+        });
       }),
       map(res => profileActions.profileLoaded({profiles: res.items}))
     );
