052a215cdc23bc5b071aa6ffe0d54ced92033639
Ilya <iosdsbb@gmail.com>
2025-12-23 18:37:59 +0300

    feat(profile): added logic with infinite scroll to search-page

--- libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
+++ libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
@@ -2,15 +2,23 @@ import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ProfileCard } from '../../ui';
 import { ProfileFiltersComponent } from '../profile-filters/profile-filters.component';
-import { selectFilteredProfiles } from '../../data';
+import {profileActions, selectFilteredProfiles} from '../../data';
+import {InfiniteScrollTriggerComponent} from '@tt/common-ui';
 
 @Component({
   selector: 'app-search-page',
-  imports: [ProfileCard, ProfileFiltersComponent],
+  imports: [ProfileCard, ProfileFiltersComponent, InfiniteScrollTriggerComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
 })
 export class SearchPageComponent {
   store = inject(Store);
   profiles = this.store.selectSignal(selectFilteredProfiles);
+  console = console;
+
+  constructor() {}
+
+  timeToFetch() {
+    this.store.dispatch(profileActions.setPage({}));
+  }
 }
