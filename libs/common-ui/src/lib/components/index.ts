2952caf3ec95a129df64a007419551305e4c61b1
Ilya <iosdsbb@gmail.com>
2026-01-10 19:07:31 +0300

    feat(common-ui): tt-input init

--- libs/common-ui/src/lib/components/index.ts
+++ libs/common-ui/src/lib/components/index.ts
@@ -2,10 +2,12 @@ import { AvatarCircleComponent } from './avatar-circle/avatar-circle.component';
 import { InfiniteScrollTriggerComponent } from './infinite-scroll-trigger/infinite-scroll-trigger.component';
 import { MessageCountComponent } from './message-count/message-count.component';
 import { SvgIconComponent } from './svg-icon/svg-icon.component';
+import { TtInputComponent } from './tt-input/tt-input.component';
 
 export {
   AvatarCircleComponent,
   InfiniteScrollTriggerComponent,
   MessageCountComponent,
-  SvgIconComponent
+  SvgIconComponent,
+  TtInputComponent,
 };
---
+++ libs/common-ui/src/lib/components/tt-input/tt-input.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'tt-input',
+  imports: [CommonModule],
+  templateUrl: './tt-input.component.html',
+  styleUrl: './tt-input.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TtInputComponent {}
