d842010b5434670a9ee502d5d87bf4f832df3020
Ilya <iosdsbb@gmail.com>
2026-01-10 19:22:28 +0300

    feat(common-ui/tt-input): added input text, placeholder

--- libs/common-ui/src/lib/components/tt-input/tt-input.component.ts
+++ libs/common-ui/src/lib/components/tt-input/tt-input.component.ts
@@ -1,11 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tt-input',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './tt-input.component.html',
   styleUrl: './tt-input.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TtInputComponent {}
+export class TtInputComponent {
+  type = input<'text' | 'password'>('text');
+  placeholder = input<string>();
+}
