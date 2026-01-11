f2ed30efdece86303e3fdad468e605fed76c18c5
Ilya <iosdsbb@gmail.com>
2026-01-11 12:51:58 +0300

    feat(common-ui): updated stack-input

--- libs/common-ui/src/lib/components/stack-input/stack-input.component.ts
+++ libs/common-ui/src/lib/components/stack-input/stack-input.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'tt-stack-input',
-  imports: [CommonModule],
+  imports: [CommonModule, SvgIconComponent],
   templateUrl: './stack-input.component.html',
   styleUrl: './stack-input.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
