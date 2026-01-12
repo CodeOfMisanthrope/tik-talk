aca8f1784c2a15ed0c443bb3c7b8dada04de9003
Ilya <iosdsbb@gmail.com>
2026-01-13 00:10:16 +0300

    feat(common-ui): added inputs for city, street, house

--- libs/common-ui/src/lib/components/address-input/address-input.component.ts
+++ libs/common-ui/src/lib/components/address-input/address-input.component.ts
@@ -1,18 +1,28 @@
-import { ChangeDetectionStrategy, Component, forwardRef, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  forwardRef,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   ControlValueAccessor,
-  FormControl,
+  FormControl, FormGroup,
   NG_VALUE_ACCESSOR,
   ReactiveFormsModule,
 } from '@angular/forms';
 import { TtInputComponent } from '../tt-input/tt-input.component';
 import { DadataService } from '../../data';
 import { debounceTime, switchMap, tap } from 'rxjs';
+import { FullAddressPipe } from '../../pipes/full-address.pipe';
+import { DadataSuggestions } from '../../data/interfaces/dadata.interface';
+import { concatAddress } from '@tt/shared';
 
 @Component({
   selector: 'tt-address-input',
-  imports: [CommonModule, TtInputComponent, ReactiveFormsModule],
+  imports: [CommonModule, TtInputComponent, ReactiveFormsModule, FullAddressPipe],
   templateUrl: './address-input.component.html',
   styleUrl: './address-input.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -30,6 +40,12 @@ export class AddressInputComponent implements ControlValueAccessor {
 
   isDropdownOpened = signal(true);
 
+  addressForm = new FormGroup({
+    city: new FormControl(''),
+    street: new FormControl(''),
+    house: new FormControl(''),
+  });
+
   suggestions$ = this.innerSearchControl.valueChanges.pipe(
     debounceTime(500),
     switchMap((val) => {
@@ -43,7 +59,7 @@ export class AddressInputComponent implements ControlValueAccessor {
 
   writeValue(city: string | null): void {
     this.innerSearchControl.patchValue(city, {
-      emitEvent: false
+      emitEvent: false,
     });
   }
 
@@ -61,11 +77,24 @@ export class AddressInputComponent implements ControlValueAccessor {
 
   onTouched() {}
 
-  onSuggestionPick(city: string) {
+  onSuggestionPick(suggest: DadataSuggestions) {
     this.isDropdownOpened.set(false);
-    this.innerSearchControl.patchValue(city, {
+    // this.innerSearchControl.patchValue(city, {
+    //   emitEvent: false,
+    // });
+    // this.onChange(city);
+    const { city, street, house } = suggest.data;
+    const address = concatAddress(city, street, house);
+
+    this.innerSearchControl.patchValue(address, {
       emitEvent: false,
     });
-    this.onChange(city);
+    this.onChange(address);
+
+    this.addressForm.patchValue({
+      city: city,
+      street: street,
+      house: house,
+    });
   }
 }
